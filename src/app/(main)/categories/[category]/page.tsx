311b95ef11f9e6f5d345f1e0491695c6d6c2d16e
jhimi01 <talukderjhimi@gmail.com>
2025-03-05 23:42:54 +0600

    issue

--- src/app/(main)/categories/[category]/page.tsx
+++ src/app/(main)/categories/[category]/page.tsx
@@ -25,14 +25,11 @@ export default async function CategoryPage({
 }: {
   params: { category: string };
 }) {
-  const { category } = params; // No need for 'await' here, it's already synchronously passed.
+  const { category } = params;
 
-  // Debugging category value
-  console.log("Fetched category:", category);
-
-  // Check available categories in the database (for debugging)
   const availableCategories = await prisma.blogPost.findMany({
     select: { category: true },
+    distinct: ['category'],
   });
   console.log("Available categories:", availableCategories);
 
@@ -40,9 +37,6 @@ export default async function CategoryPage({
     where: { category },
   });
 
-  console.log('filtered blogs:', blogs);
-
-  // Check if no blogs are found
 
   if (!blogs) {
     return (
@@ -59,78 +53,71 @@ export default async function CategoryPage({
     );
   }
 
-  // if (blogs.length === 0) {
-  //   return (
-  //     <div className="md:flex gap-5 mt-10 container mx-auto text-3xl text-gray-900 items-center justify-center">
-  //       There is no blog post available
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="container mx-auto my-8">
       <div className="flex gap-6">
         <div className="md:w-[75%] w-full md:px-0 px-5 space-y-5">
-{blogs.length === 0 ? <div className="md:flex gap-5 mt-10 container mx-auto text-3xl text-gray-900 items-center justify-center">
-        There is no blog post available
-      </div> :  <div className="space-y-2">
-            {blogs.map((blog) => (
-              <div
-                key={blog.id}
-                className="hover:bg-accent  border bg-accent md:bg-transparent "
-              >
-                <div className="md:flex gap-2">
-                  <Image
-                    width={800}
-                    height={800}
-                    src={blog?.image || "/placeholder.jpg"} // Fallback image if no image exists
-                    alt="blog post"
-                    className="md:w-48"
-                    priority
-                  />
-                  <div className="space-y-2 md:space-y-3 p-5 w-full my-2 md:my-0">
-                    <h2 className="md:text-2xl text-xl hover:text-secondary text-black">
-                      {blog.title}
-                    </h2>
-                    <div className="md:flex gap-4">
-                      <div className="flex text-gray-400 items-center gap-1 text-sm">
-                        <CalendarDays size={16} strokeWidth={1.75} />
-                        <p>{new Date(blog.date).toLocaleDateString()}</p>
+          {blogs.length === 0 ? (
+            <div className="md:flex gap-5 mt-10 container mx-auto text-3xl text-gray-900 items-center justify-center">
+              There is no blog post available
+            </div>
+          ) : (
+            <div className="space-y-2">
+              {blogs.map((blog) => (
+                <div
+                  key={blog.id}
+                  className="hover:bg-accent  border bg-accent md:bg-transparent "
+                >
+                  <div className="md:flex gap-2">
+                    <Image
+                      width={800}
+                      height={800}
+                      src={blog?.image || "/placeholder.jpg"} // Fallback image if no image exists
+                      alt="blog post"
+                      className="md:w-48"
+                      priority
+                    />
+                    <div className="space-y-2 md:space-y-3 p-5 w-full my-2 md:my-0">
+                      <h2 className="md:text-2xl text-xl hover:text-secondary text-black">
+                        {blog.title}
+                      </h2>
+                      <div className="md:flex gap-4">
+                        <div className="flex text-gray-400 items-center gap-1 text-sm">
+                          <CalendarDays size={16} strokeWidth={1.75} />
+                          <p>{new Date(blog.date).toLocaleDateString()}</p>
+                        </div>
+                        <div className="flex text-gray-400 items-center gap-1 text-sm">
+                          <File size={16} strokeWidth={1.75} />
+                          <p>{blog.category}</p>
+                        </div>
+                        <div className="flex text-gray-400 items-center gap-1 text-sm">
+                          <ThumbsUp size={16} strokeWidth={1.75} />
+                          <p>{blog.likes}</p>
+                        </div>
                       </div>
-                      <div className="flex text-gray-400 items-center gap-1 text-sm">
-                        <File size={16} strokeWidth={1.75} />
-                        <p>{blog.category}</p>
+                      <div>
+                        <p className="text-base text-gray-600">{blog?.desc}</p>
                       </div>
-                      <div className="flex text-gray-400 items-center gap-1 text-sm">
-                        <ThumbsUp size={16} strokeWidth={1.75} />
-                        <p>{blog.likes}</p>
+                      <hr />
+                      <div className="md:flex justify-between text-gray-400">
+                        <h3 className="text-sm flex items-center gap-1">
+                          <CircleUserRound size={16} strokeWidth={1.75} />{" "}
+                          {blog?.userEmail}
+                        </h3>
+                        <Link
+                          className="text-secondary text-sm font-semibold flex items-center"
+                          href={`/blogs/${blog.id}`}
+                        >
+                          Read More
+                          <ChevronRight size={16} strokeWidth={2} />
+                        </Link>
                       </div>
-                    </div>
-                    <div>
-                      <p className="text-base text-gray-600">{blog?.desc}</p>
-                    </div>
-                    <hr />
-                    <div className="md:flex justify-between text-gray-400">
-                      <h3 className="text-sm flex items-center gap-1">
-                        <CircleUserRound size={16} strokeWidth={1.75} />{" "}
-                        {blog?.userEmail}
-                      </h3>
-                      <Link
-                        className="text-secondary text-sm font-semibold flex items-center"
-                        href={`/blogs/${blog.id}`}
-                      >
-                        Read More
-                        <ChevronRight size={16} strokeWidth={2} />
-                      </Link>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>}
-
-
-         
+              ))}
+            </div>
+          )}
         </div>
 
         {/* leftsidebar */}
